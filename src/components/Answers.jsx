4db30c5771ca94a95079b30c6512e80d95b6c156
kamrulSoppiya <kamrul.soppiya@gmail.com>
2023-09-20 17:29:21 +0600

    x

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -5,6 +5,15 @@ export default function Answers() {
   return (
     <div className={classes.answers}>
       <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
+      <Checkbox className={classes.answer} text="ডেমো কোর্সন , কালম এন্ড কূল  " />
     </div>
   );
 }
---
+++ src/components/Quiz/QuizDataClinet.jsx
@@ -0,0 +1,13 @@
+import QuizHook from "../../Hooks/QuizHook";
+import _ from "lodash";
+
+const reducre
+export default function QuizDataClient(){
+    const {loading, error, question} = QuizHook();
+
+    return(
+        <>
+
+        </>
+    );
+}
